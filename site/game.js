e02db531148c925375b3006ab28e679fa23bf274
Rob Ashton <robashton@codeofrob.com>
2012-03-31 08:37:45 +0100

    Basic libraries added, now for some fluff generation

--- site/game.js
+++ site/game.js
@@ -1,6 +1,155 @@
 (function() {
 
+  var EventContainer = function(defaultContext) {
+    this.handlers = [];
+    this.defaultContext = defaultContext;
+  }; 
   
+  EventContainer.prototype = {
+    raise: function(source, data) {
+     var handlerLength = this.handlers.length;
+     for(var i = 0; i < handlerLength; i++) {
+        var handler = this.handlers[i];
+        handler.method.call(handler.context || this.defaultContext, data, source);   
+     }
+    },
+    add: function(method, context) {
+      this.handlers.push({
+        method: method,
+        context: context      
+      });
+    },
+    remove: function(method, context) {
+      this.handlers = _(this.handlers).filter(function(item) {
+        return item.method !== method || item.context !== context;
+      });
+    }
+  };
+
+   var Eventable = function() {
+    this.eventListeners = {};
+    this.allContainer = new EventContainer(this);
+    this.eventDepth = 0;
+  };
+  
+  Eventable.prototype = {
+    autoHook: function(container) {
+      for(var key in container) { 
+        if(key.indexOf('on') === 0) {
+          this.on(key.substr(2), container[key], container);
+        }   
+      }
+    },
+    autoUnhook: function(container) {
+      for(var key in container) { 
+        if(key.indexOf('on') === 0) {
+          this.off(key.substr(2), container[key], container);
+        }   
+      }
+    },
+    once: function(eventName, callback, context) {
+      var self = this;
+      var wrappedCallback = function(data, sender) {
+        callback.call(this, data, sender);
+        self.off(eventName, wrappedCallback, context);
+      };
+      this.on(eventName, wrappedCallback, context);
+    },
+    
+    on: function(eventName, callback, context) {
+      this.eventContainerFor(eventName).add(callback, context);
+    },
+    
+    off: function(eventName, callback, context) {
+      this.eventContainerFor(eventName).remove(callback, context);
+    },
+
+    onAny: function(callback, context) {
+      this.allContainer.add(callback, context);
+    },
+
+    raise: function(eventName, data, sender) {
+      this.audit(eventName, data);
+      var container = this.eventListeners[eventName];
+
+      if(container)
+        container.raise(sender || this, data);
+      
+      this.allContainer.raise(sender || this, {
+        event: eventName,
+        data: data
+      });
+    },
+    
+    audit: function(eventName, data) {
+      
+    },
+
+    eventContainerFor: function(eventName) {
+      var container = this.eventListeners[eventName];
+      if(!container) {
+        container =  new EventContainer(this);
+        this.eventListeners[eventName] = container;
+      }
+      return container;
+    }
+  };
+
+  var Scene = function() {
+    this.entities = {};
+  };
+
+  Scene.prototype = function() {
+    add: function(entity) {
+      this.entities[entity.id] = entity;
+    },
+    remove: function(entity) {
+      delete this.entities[entity.id];
+    },
+    render: function(context) {
+      this.each(function(entity) {
+        if(entity.render) entity.render(context);
+      });
+    },
+    tick: function() {
+      this.each(function(entity) {
+        if(entity.tick) entity.tick();
+      });
+    },
+    each: function(cb) {
+      for(var key in this.entities) {
+        cb(this.entities[key]);
+      }
+    }
+  };
+
+
+  var Game = function() {
+    this.scene = new Scene();
+    this.renderer = new Renderer('game');
+  };
+
+  Game.prototype = function() {
+    start: function() {
+      this.scene.add(new FluffGenerator());
+
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }).call(this);
